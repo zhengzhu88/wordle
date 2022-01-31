e1059c6b6e296b9f66864b5d7ae345d9ce42991e
Zheng Zhu <zhengzhu88@gmail.com>
2022-01-31 12:23:38 -0800

    double letters work now

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from collections import Counter
 from enum import Enum
-from typing import List
+from typing import List, Dict
 
 import re
 
@@ -42,7 +42,7 @@ class Position:
 # TODO: Add a UI for setting status instead of doing text input.
 class Session:
     def __init__(self):
-        self.wrong_pos_letters = {}
+        self.known_letters = {}
         self.positions: List[Position] = [Position(), Position(), Position(), Position(), Position()]
         with open("words_alpha_length_5.txt") as dictionary:
             self.dictionary: str = dictionary.read()
@@ -59,8 +59,11 @@ class Session:
             if not self._process_guess(guess, result):
                 continue
             pattern: str = self._generate_regex()
+            # Filter out words based on letter position.
             regex_matches: List[str] = re.findall(pattern, self.dictionary)
-            matches: List[str] = [word for word in regex_matches if self._contains_wrong_pos_letters(word)]
+            # Filter out words that don't use all known letters.
+            matches: List[str] = \
+                [word for word in regex_matches if self._word_contains_known_letters(word, self.known_letters)]
             print(f"Found {len(matches)} matches:")
             for match in matches:
                 print(match.strip())
@@ -70,9 +73,9 @@ class Session:
             iteration += 1
         print("Better luck next time!")
 
-    def _contains_wrong_pos_letters(self, word):
+    def _word_contains_known_letters(self, word: str, known_letters: Dict[str, int]) -> bool:
         counter = Counter(word)
-        for letter, count in self.wrong_pos_letters.items():
+        for letter, count in known_letters.items():
             if count > counter[letter]:
                 return False
         return True
@@ -97,32 +100,25 @@ class Session:
         statuses: List[GuessStatus] = [self._map_letter_to_status(char) for char in result]
         if GuessStatus.INVALID_CHARACTER in statuses:
             return False
-        wrong_pos_letters_for_word = {}
+        known_letters_for_word = {}
         for i in range(len(guess)):
             letter = guess[i]
             status: GuessStatus = self._map_letter_to_status(result[i])
             if status == GuessStatus.CONFIRMED:
-                if letter in self.wrong_pos_letters:
-                    self.wrong_pos_letters[letter] -= 1
-                    if self.wrong_pos_letters[letter] == 0:
-                        del self.wrong_pos_letters[letter]
                 self.positions[i].confirm_letter(letter=letter)
+                known_letters_for_word[letter] = known_letters_for_word.get(letter, 0) + 1
             elif status == GuessStatus.DECONFIRMED:
                 for pos_i in range(len(self.positions)):
                     self.positions[pos_i].deconfirm_letter(letter=letter)
             elif status == GuessStatus.WRONG_POSITION:
                 self.positions[i].deconfirm_letter(letter=letter)
-                if letter not in wrong_pos_letters_for_word:
-                    wrong_pos_letters_for_word[letter] = 0
-                wrong_pos_letters_for_word[letter] += 1
-        # Merge the sets of wrongly positioned letters. This has a bug in the case where the history has seen more
-        # occurrences of a letter than the current guess, e.g.
-        # self.wrong_pos_letters -> 'n': 2
-        # wrong_pos_letters_for_word -> 'n': 1
-        # This would overwrite the former value with the latter value due to key collision, but this shouldn't happen
-        # if each guess is taken strictly from the list.
-        self.wrong_pos_letters = {**self.wrong_pos_letters, **wrong_pos_letters_for_word}
+                known_letters_for_word[letter] = known_letters_for_word.get(letter, 0) + 1
+        self._merge_known_letters(known_letters_for_word)
         return True
+
+    def _merge_known_letters(self, known_letters_for_word: Dict[str, int]):
+        for letter, count in known_letters_for_word.items():
+            self.known_letters[letter] = max(count, self.known_letters.get(letter, 0))
 
     def _map_letter_to_status(self, char: str) -> GuessStatus:
         if len(char) != 1:
